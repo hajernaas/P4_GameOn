c280718e8dc075910650339daac766e190396068
hajernaas <hnaas2702@gmail.com>
2023-09-20 22:05:32 +0200

    open and close  modal

--- js/modal.js
+++ js/modal.js
@@ -2,20 +2,32 @@
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// ouvrir le modal au click sur le boutton 'je m'inscris'
+modalBtn.forEach((btn) =>
+	btn.addEventListener("click", () => {
+		if (modalbg === null) throw new Error("modal n'existe pas");
+		//modalbg.style.display = "block";
+		modalbg.classList.add("active");
+	})
+);
 
-// launch modal form
-function launchModal() {
+// fermer le modal au click sur le bouton X
+closeModal.addEventListener("click", () => {
+	if (modalbg === null) throw new Error("modal n'existe pas");
+	modalbg.classList.remove("active");
+});
+
+//fonction pour ouvrir le modal
+/*function launchModal() {
+	if (modalbg === null) throw new Error("modal n'existe pas");
 	modalbg.style.display = "block";
-}
+}*/
+
 //function editNav
 function editNav() {
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+	const myNavbar = document.getElementById("myTopnav");
+	if (myNavbar === null) throw new Error(" menu de navigation n'existe pas ");
+	myNavbar.classList.toggle("responsive");
 }
